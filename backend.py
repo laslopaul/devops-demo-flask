23bbdd83cfa365e532bb5d372a97bd87635813d4
laslopaul <96680549+laslopaul@users.noreply.github.com>
2023-11-13 18:51:22 +0400

    Improve loggng for backend module

--- backend.py
+++ backend.py
@@ -4,11 +4,22 @@ from time import time, strftime, gmtime
 from random import randint
 from os import getenv
 from hashlib import sha256
+import logging
 
-db = pw.MySQLDatabase(getenv("MYSQL_DB"), user=getenv("MYSQL_USER"),
-                      password=getenv("MYSQL_PASSWD"),
-                      host=getenv("MYSQL_HOST"),
-                      port=int(getenv("MYSQL_PORT")))
+# Get Flask CLI logger
+logger = logging.getLogger("devops_demo_cli")
+
+
+try:
+    sql_port = int(getenv("MYSQL_PORT"))
+except TypeError:
+    logger.warning("MySQL port is not defined, using default 3306")
+    sql_port = 3306
+finally:
+    db = pw.MySQLDatabase(getenv("MYSQL_DB"), user=getenv("MYSQL_USER"),
+                          password=getenv("MYSQL_PASSWD"),
+                          host=getenv("MYSQL_HOST"),
+                          port=sql_port)
 
 
 class BaseModel(pw.Model):
@@ -28,21 +39,21 @@ def db_init(recreate=False) -> bool:
     table_existed_before = False
 
     if not db.table_exists("fortune"):
-        print("Creating Fortune table...")
+        logger.info("Creating Fortune table...")
         with db.connection_context():
             db.create_tables([Fortune])
-            print("Fortune table created.")
+            logger.info("Fortune table created.")
 
     elif db.table_exists("fortune") and recreate:
-        print("Dropping Fortune table...")
+        logger.info("Dropping Fortune table...")
         with db.connection_context():
             db.drop_tables([Fortune])
             db.create_tables([Fortune])
-            print("Empty fortune table recreated.")
+            logger.info("Empty fortune table recreated.")
 
     else:
         table_existed_before = True
-        print("Fortune table already exists.")
+        logger.info("Fortune table already exists.")
 
     return table_existed_before
 
@@ -56,34 +67,32 @@ def import_data(filename: str) -> None:
     entries_count = len(entries)
     start = time()
     for entry in entries:
-        m = sha256(entry.encode('UTF-8'))
+        m = sha256(entry.encode("UTF-8"))
         try:
             Fortune.create(text=entry, date_added=datetime.now(),
                            fortune_hash=m.hexdigest())
         except pw.IntegrityError:
-            wrn_color = "\033[93m"
-            wrn_msg = "{}{}\t[WRN]\tEntry {} already exists in the database."
-            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
-            print(wrn_msg.format(wrn_color, timestamp, i))
+            logger.warning(f"Entry {i} already exists in the database")
             entries_count -= 1
         i += 1
-        inf_color = "\033[0;37m"
-        inf_msg = "{}{}\t[INF]\tProcessing entry {} of {} ({}%)"
+        inf_msg = "Processing entry {} of {} ({}%)"
         progress = format(i / len(entries) * 100, ".02f")
-        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
-        print(inf_msg.format(inf_color, timestamp, i, len(entries), progress))
+        logger.info(inf_msg.format(i, len(entries), progress))
 
     end = time()
     elapsed = end - start
     elapsed_fmt = strftime("%H:%M:%S", gmtime(elapsed))
     speed = format(len(entries) / elapsed, ".02f")
-    print(f"\nImported {entries_count} entries in {elapsed_fmt} ({speed} eps)")
-    print(f"Duplicates found: {len(entries) - entries_count}")
+    logger.info(
+        f"Imported {entries_count} entries in {elapsed_fmt} ({speed} eps)")
+    logger.info(f"Duplicates found: {len(entries) - entries_count}")
 
 
 def read_fortune() -> str:
     """Read a random fortune from the database"""
     with db.atomic():
         max_index = Fortune.select().count()
-        fortune = Fortune.get_by_id(randint(1, max_index))
+        fortune_id = randint(1, max_index)
+        fortune = Fortune.get_by_id(fortune_id)
+    logger.info(f"Read fortune #{fortune_id} from the database")
     return fortune.text
